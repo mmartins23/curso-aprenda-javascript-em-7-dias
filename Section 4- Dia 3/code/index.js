6d69d44a5f388297d2036cfcc2ef4d9845e2b581
Marlon Martins <marlon-martins@outlook.com>
2024-05-06 10:45:06 +0200

    48. Criando um Objeto

---
+++ Section 4- Dia 3/code/index.js
@@ -0,0 +1,13 @@
+// 48. Criando um Objeto
+
+const book = {
+    bookTitle: 'Atomic Habits',
+    bookAuthor: 'James Clear',
+    bookPages: 306,
+    bookChapters: {
+        chap1: 'Fundamentals',
+        chap2: '1st Law'
+    }
+}
+
+console.log(book);
